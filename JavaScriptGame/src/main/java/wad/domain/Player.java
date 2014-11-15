cddb480e066b60385ac511f1b927c96e4996eef5
Uulari <uulari@gmail.com>
2014-11-15 22:41:19 +0200

    - lets try again

---
+++ JavaScriptGame/src/main/java/wad/domain/Player.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.domain;
+
+/**
+ *
+ * @author User
+ */
+public class Player {
+
+    private int score;
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
