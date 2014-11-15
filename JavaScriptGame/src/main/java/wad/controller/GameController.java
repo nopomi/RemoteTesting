bf9f64300a97719dce9dc24428f2bca8575ddca5
mide <mide@mide-N56VM>
2014-11-15 23:08:39 +0200

    finally working base model of our game, no answering yet tho 2

--- JavaScriptGame/src/main/java/wad/controller/GameController.java
+++ JavaScriptGame/src/main/java/wad/controller/GameController.java
@@ -8,17 +8,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.Game;
 import wad.domain.Guest;
 
 @Controller
 @RequestMapping("/guests")
 public class GameController {
-
-    private final List<Guest> guests = new ArrayList<>();
+    
+    private final Game game = new Game();
 
     @RequestMapping(method = RequestMethod.GET)
     public String list(Model model) {
-        model.addAttribute("guests", guests);
+        model.addAttribute("greeting", "Do you want to play a game?");
+        List<String> questions = game.getQuestions();
+        model.addAttribute("questions", questions);
         return "/WEB-INF/views/page.jsp";
     }
     
